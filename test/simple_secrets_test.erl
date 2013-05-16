1a59ede8d591b663c6259255aa9ee5cb141b17f2
Cameron Bytheway <cameron@nujii.com>
2013-05-16 07:09:25 -0600

    Include proper tests

--- test/simple_secrets_test.erl
+++ test/simple_secrets_test.erl
@@ -2,10 +2,20 @@
 
 -compile(export_all).
 
+-include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -define (MASTER_KEY, <<"12345678901234567890123456789012">>).
 -define (OTHER_MASTER_KEY, <<"09876543210987654321098765432109">>).
+
+prop_valid_messages()->
+  Sender = simple_secrets:init(?MASTER_KEY),
+  ?FORALL(Msg, binary(),
+    begin
+      EncMessage = simple_secrets:pack(Msg, Sender),
+      DecMessage = simple_secrets:unpack(EncMessage, Sender),
+      Msg =:= DecMessage
+    end).
 
 recover_text_test()->
   Message = <<"this is a secret message">>,
@@ -21,3 +31,8 @@ unrecoverable_text_test()->
   EncMessage = simple_secrets:pack(Message, Sender),
   DecMessage = simple_secrets:unpack(EncMessage, Sender2),
   ?assertEqual(false, DecMessage).
+
+proper_test_() ->
+    [{atom_to_list(F),
+      fun () -> ?assert(proper:quickcheck(?MODULE:F(), [long_result])) end}
+     || {F, 0} <- ?MODULE:module_info(exports), F > 'prop_', F < 'prop`'].
